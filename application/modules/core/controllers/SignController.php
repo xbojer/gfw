e7ee4e48ac29f89528288aa128aea8fd0ac293c5
Patryk Kala <kallosz@clik.pl>
2011-10-10 21:18:34 +0200

    session handler for zend_auth

--- application/modules/core/controllers/SignController.php
+++ application/modules/core/controllers/SignController.php
@@ -4,10 +4,11 @@ class SignController extends Core_Library_Controller_Action_Abstract {
 		$this->redirect('index', 'index', 'core');
 	}
 	public function upAction() {
-		#throw new Exception('Sign up action not yet implemented');
+		throw new Exception('Sign up action not yet implemented');
 	}
 	public function inAction() {
-		if(!$this->isAjaxRequest) {
+		$user = new Zend_Session_Namespace('Zend_Auth');
+		if(!$this->isAjaxRequest || $user->isSignIn === true) {
 			$this->redirect('index', 'index', 'core');
 		}
 		throw new Exception('Sign in action not yet implemented');
