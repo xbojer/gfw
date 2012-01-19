d20b7dcc1c162f79fac08672c6f32d48d6701b94
xbojer <fbojer@gmail.com>
2012-01-19 19:18:54 +0100

    Update web/app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -14,6 +14,8 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
 #    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
+var_dump($_GET);
+
 require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 
